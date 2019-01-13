8415bc02c8012e5bdedcb99889ff252cf8783256
miasevoli <miasevoli.frc@gmail.com>
2019-01-13 12:37:05 -0500

    Added OSC lib

--- RobotCode/src/main/java/frc/team1071/robot/Robot.java
+++ RobotCode/src/main/java/frc/team1071/robot/Robot.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.revrobotics.*;
-
+import com.illposed.osc.*;
 import static com.revrobotics.CANSparkMaxLowLevel.MotorType.kBrushless;
 import static java.lang.Math.*;
 
@@ -17,107 +17,107 @@ import static java.lang.Math.*;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+    private static final String kDefaultAuto = "Default";
+    private static final String kCustomAuto = "My Auto";
+    private String m_autoSelected;
+    private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  //Setup the joysticks
-  Joystick driverJoystick = new Joystick(0);
-  Joystick switchesJoystick = new Joystick(1);
+    //Setup the joysticks
+    Joystick driverJoystick = new Joystick(0);
+    Joystick switchesJoystick = new Joystick(1);
 
-  //Setup motors
-  CANSparkMax leftMaster = new CANSparkMax(0, kBrushless);
-  CANSparkMax leftSlavePrimary = new CANSparkMax(1, kBrushless);
-  CANSparkMax leftSlaveSecondary = new CANSparkMax(2, kBrushless);
-  CANSparkMax rightMaster = new CANSparkMax(13, kBrushless);
-  CANSparkMax rightSlavePrimary = new CANSparkMax(14, kBrushless);
-  CANSparkMax rightSlaveSecondary = new CANSparkMax(15, kBrushless);
+    //Setup motors
+    CANSparkMax leftMaster = new CANSparkMax(0, kBrushless);
+    CANSparkMax leftSlavePrimary = new CANSparkMax(1, kBrushless);
+    CANSparkMax leftSlaveSecondary = new CANSparkMax(2, kBrushless);
+    CANSparkMax rightMaster = new CANSparkMax(13, kBrushless);
+    CANSparkMax rightSlavePrimary = new CANSparkMax(14, kBrushless);
+    CANSparkMax rightSlaveSecondary = new CANSparkMax(15, kBrushless);
 
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-    leftSlavePrimary.follow(leftMaster);
-    leftSlaveSecondary.follow(leftMaster);
-    rightSlavePrimary.follow(rightMaster);
-    rightSlaveSecondary.follow(rightMaster);
-    rightMaster.setInverted(true);
-    rightSlavePrimary.setInverted(true);
-    rightSlaveSecondary.setInverted(true);
-  }
-
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
-
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
-
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
+    /**
+     * This function is run when the robot is first started up and should be
+     * used for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+        m_chooser.addOption("My Auto", kCustomAuto);
+        SmartDashboard.putData("Auto choices", m_chooser);
+        leftSlavePrimary.follow(leftMaster);
+        leftSlaveSecondary.follow(leftMaster);
+        rightSlavePrimary.follow(rightMaster);
+        rightSlaveSecondary.follow(rightMaster);
+        rightMaster.setInverted(true);
+        rightSlavePrimary.setInverted(true);
+        rightSlaveSecondary.setInverted(true);
     }
-  }
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-    double driverVertical = QuickMaths.normalizeJoystickWithDeadband(driverJoystick.getY(), 0.1);
-    double driverTwist = QuickMaths.normalizeJoystickWithDeadband(driverJoystick.getZ(), 0.1);
+    /**
+     * This function is called every robot packet, no matter the mode. Use
+     * this for items like diagnostics that you want ran during disabled,
+     * autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before
+     * LiveWindow and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+    }
+
+    /**
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable
+     * chooser code works with the Java SmartDashboard. If you prefer the
+     * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+     * getString line to get the auto name from the text box below the Gyro
+     *
+     * <p>You can add additional auto modes by adding additional comparisons to
+     * the switch structure below with additional strings. If using the
+     * SendableChooser make sure to add them to the chooser code above as well.
+     */
+    @Override
+    public void autonomousInit() {
+        m_autoSelected = m_chooser.getSelected();
+        // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+        System.out.println("Auto selected: " + m_autoSelected);
+    }
+
+    /**
+     * This function is called periodically during autonomous.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        switch (m_autoSelected) {
+            case kCustomAuto:
+                // Put custom auto code here
+                break;
+            case kDefaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+    }
+
+    /**
+     * This function is called periodically during operator control.
+     */
+    @Override
+    public void teleopPeriodic() {
+        double driverVertical = QuickMaths.normalizeJoystickWithDeadband(driverJoystick.getY(), 0.1);
+        double driverTwist = QuickMaths.normalizeJoystickWithDeadband(driverJoystick.getZ(), 0.1);
 //    double leftPower = min(max(driverVertical + driverTwist, -1), 1);
 //    double rightPower = min(max(driverVertical - driverTwist, -1), 1);
-    DriveHelper driveHelper = new DriveHelper();
-    DriveMotorValues vals = driveHelper.calculateOutput(driverVertical, driverTwist, driverJoystick.getTrigger(), false);
-    leftMaster.set(vals.leftDrive);
-    rightMaster.set(vals.rightDrive);
-  }
+        DriveHelper driveHelper = new DriveHelper();
+        DriveMotorValues vals = driveHelper.calculateOutput(driverVertical, driverTwist, driverJoystick.getTrigger(), false);
+        leftMaster.set(vals.leftDrive);
+        rightMaster.set(vals.rightDrive);
+    }
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+    /**
+     * This function is called periodically during test mode.
+     */
+    @Override
+    public void testPeriodic() {
+    }
 }
